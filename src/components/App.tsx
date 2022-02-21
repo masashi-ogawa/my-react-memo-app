db68bf22e319116b6e491a66de5a8185566cbfd1
Masashi Ogawa <masas.ogawa@gmail.com>
2022-02-22 01:01:02 +0900

    add custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,29 @@
 import { ChangeEvent, useState, FC, useCallback } from 'react';
 import styled from 'styled-components';
 import { MemoList } from './MemoList';
+import { useMemoList } from '../hooks/useMemoList';
 
 export const App: FC = () => {
+  const { memos, addTodo, deleteTodo } = useMemoList();
   const [text, setText] = useState<string>("");
-  const [memos, setMemos] = useState<string[]>([]);
 
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
   const onClickAdd = () => {
-    const newMemos = [...memos];
-    newMemos.push(text);
-    setMemos(newMemos);
+    addTodo(text);
     setText("");
   };
 
   const onClickDelete = useCallback((index: number) => {
-    const newMemos = [...memos];
-    newMemos.splice(index, 1);
-    setMemos(newMemos);
-  }, [memos]);
+    deleteTodo(index);
+  }, [deleteTodo]);
 
   return (
     <div>
       <h1>簡単メモアプリ</h1>
-      <input type="text" value={text} onChange={onChangeText} />
+      <input type="text" value={text} onChange={onChangeText}/>
       <SButton onClick={onClickAdd}>追加</SButton>
-      <MemoList memos={memos} onClickDelete={onClickDelete} />
+      <MemoList memos={memos} onClickDelete={onClickDelete}/>
     </div>
   )
 }
